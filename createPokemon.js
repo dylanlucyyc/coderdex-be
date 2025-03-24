aabd5b0f1758a0249c1f163fc709776e540d347a
Dylan Luc <dylanlucyyc@gmail.com>
2025-03-23 21:16:17 -0600

    Take care of the data and images ensuring there is no duplication

---
+++ createPokemon.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const fs = require("fs");
+const csv = require("csvtojson");
+
+const url = process.env.URL;
+
+const createPokemon = async () => {
+  let newData = await csv().fromFile("Pokemon.csv");
+  // Remove duplicates based on ID while preserving the full object structure
+  newData = Array.from(new Set(newData.map((e) => e["#"])))
+    .map((id) => newData.find((e) => e["#"] === id))
+    .map((e) => ({
+      id: e["#"],
+      name: e.Name,
+      type: [e["Type 1"], e["Type 2"]].filter(Boolean), // Remove null/undefined types
+      url: url + "images/" + e["#"],
+    }))
+    .filter((e) => e.name);
+
+  let data = JSON.parse(fs.readFileSync("db.json"));
+
+  data = {
+    data: newData,
+    totalPokemons: newData.length, // Count after filtering
+  };
+
+  fs.writeFileSync("db.json", JSON.stringify(data, null, 2));
+  console.log(data);
+};
+
+createPokemon();
